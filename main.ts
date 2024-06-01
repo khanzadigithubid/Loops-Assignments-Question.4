d0e7353623fe565b9a909cf6873c140f5e5c8d65
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-06-02 00:17:12 +0500

    This is Loops Assignments Question 4.Exploring Objects with for...in Loop
    This is Loops Assignments Question 4.Exploring Objects with for...in Loop

---
+++ main.ts
@@ -0,0 +1,23 @@
+//4.Exploring Objects with for...in Loop
+
+//Objective: Practice working with objects in TypeScript and iterating over their properties using a for...in loop.
+//Instructions:
+//1. Create a simple object with three items:
+//o Define an object called myObject with three properties: item1, item2, and item3, each with corresponding string values.
+//2. Use a for...in loop to get properties' names and values from the object:
+//o Iterate through the properties of myObject using a for...in loop.
+//o Inside the loop, print each property's name and its corresponding value to the console.
+
+// Create a simple object with three items
+  let person = {
+    firstName: "Khanzadi",
+    lastName: "Wazir Ali",
+    age: 20,
+  };
+  
+  // Use a for...in loop to get properties' names and values from the object
+  for (let property in person) {
+    console.log(person)
+    console.log(`Property: ${property}, Value: ${person[property]}`);
+    console.log(`Property: ${property}, Name: ${person[property]}`);
+  }
